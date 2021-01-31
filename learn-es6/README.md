f1d175801fa9fed0f8d80a8e8728782ac1712fcc
yaolei <yaolei@meituan.com>
2021-01-31 10:32:32 +0800

    doc(learn-es6): 增加README.md，并补充arrow_functions.test.js注释为什么要加括号的原因。

---
+++ learn-es6/README.md
@@ -0,0 +1,5 @@
+# 说明
+学习 ES6 的新语法，主要参考 [ECMAScript 6 — New Features](http://es6-features.org/)。另外一个比较好的参考资料是阮一峰的 [ES6 入门教程](https://es6.ruanyifeng.com/)。
+
+# 运行
+主要内容都在 tests 目录下，可通过 `npm run test` 运行，应该都是测试通过的结果。
--- learn-es6/tests/arrow_functions.test.js
+++ learn-es6/tests/arrow_functions.test.js
@@ -3,7 +3,8 @@ test('expression bodies', () => {
     let odds = evens.map(v => v + 1)
     expect(odds).toEqual([1, 3, 5, 7])
 
-    // 为什么必须用括号括起来？
+    // 必须用括号括起来，否则会解释为语句块，报错。
+    // 参考：https://es6.ruanyifeng.com/#docs/function#%E7%AE%AD%E5%A4%B4%E5%87%BD%E6%95%B0
     let pairs = evens.map(v => ({ even: v, odd: v + 1 }))
     expect(pairs).toEqual([
         { even: 0, odd: 1 }, { even: 2, odd: 3 },
