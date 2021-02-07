04928bc0fe4c3fff2e3d20b6e1f3d3164e9b1d56
yaolei <yaolei@meituan.com>
2021-02-07 23:57:33 +0800

    feat(learn-es6): class inheritance from expressions
    http://es6-features.org/#ClassInheritanceFromExpressions

--- learn-es6/tests/classes.test.js
+++ learn-es6/tests/classes.test.js
@@ -43,3 +43,60 @@ test('class inheritance', () => {
     expect(rec.width).toBe(10)
     expect(rec.height).toBe(5)
 })
+
+test('class inheritance, from expressions', () => {
+    var aggregation = (baseClass, ...mixins) => {
+        let base = class _Combined extends baseClass {
+            constructor(...args) {
+                super(...args);
+                mixins.forEach((mixin) => {
+                    mixin.prototype.initializer.call(this);
+                });
+            }
+        };
+        let copyProps = (target, source) => {
+            Object.getOwnPropertyNames(source)
+                .concat(Object.getOwnPropertySymbols(source))
+                .forEach((prop) => {
+                    if (prop.match(/^(?:constructor|prototype|arguments|caller|name|bind|call|apply|toString|length)$/))
+                        return
+                    Object.defineProperty(target, prop, Object.getOwnPropertyDescriptor(source, prop))
+                })
+        }
+        mixins.forEach((mixin) => {
+            copyProps(base.prototype, mixin.prototype);
+            copyProps(base, mixin);
+        });
+        return base;
+    };
+
+    class Colored {
+        initializer() { this._color = "white"; }
+        get color() { return this._color; }
+        set color(v) { this._color = v; }
+    }
+
+    class ZCoord {
+        initializer() { this._z = 0; }
+        get z() { return this._z; }
+        set z(v) { this._z = v; }
+    }
+
+    class Shape {
+        constructor(x, y) { this._x = x; this._y = y; }
+        get x() { return this._x; }
+        set x(v) { this._x = v; }
+        get y() { return this._y; }
+        set y(v) { this._y = v; }
+    }
+
+    class Rectangle extends aggregation(Shape, Colored, ZCoord) { }
+
+    var rect = new Rectangle(7, 42);
+    rect.z = 1000;
+    rect.color = "red";
+    expect(rect.x).toBe(7)
+    expect(rect.y).toBe(42)
+    expect(rect.z).toBe(1000)
+    expect(rect.color).toBe('red')
+})
