672b8e7950a442d95c9100cc93e0652943ef51c2
yaolei <yaolei@meituan.com>
2021-02-16 21:05:31 +0800

    fix(learn-es6): commit modules.test.js 的变更.

--- learn-es6/tests/modules.test.js
+++ learn-es6/tests/modules.test.js
@@ -3,8 +3,3 @@ test('import * from math', () => {
     expect(math.sum(1, 2)).toBe(3)
     expect(math.PI).toBe(3.141593)
 })
-
-import { sum, PI } from "../src/lib/math"
-test('import { sum, PI } from math', () => {
-    expect(sum(PI, PI)).toBe(3.141593 * 2)
-})
