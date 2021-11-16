0b4515a08272e1e5e57b3ac515a47fb9f2ce80e5
yaolei <yaolei@meituan.com>
2021-11-16 23:39:17 +0800

    fig2-2 引入Party

--- analysis-pattern/src/main/java/accountability/Person.java
+++ analysis-pattern/src/main/java/accountability/Person.java
@@ -2,26 +2,10 @@ package accountability;
 
 import com.google.common.base.MoreObjects;
 
-public class Person {
-    private final String name;
-    private final TelephoneNumber phone;
-    private final Address address;
-    private final Email email;
+public class Person extends Party {
 
     public Person(String name, TelephoneNumber phone, Address address, Email email) {
-        this.name = name;
-        this.phone = phone;
-        this.address = address;
-        this.email = email;
+        super(name, phone, address, email);
     }
 
-    @Override
-    public String toString() {
-        return MoreObjects.toStringHelper(this)
-                .add("name", name)
-                .add("phone", phone)
-                .add("address", address)
-                .add("email", email)
-                .toString();
-    }
 }
--- analysis-pattern/src/test/java/accountability/PartyTest.java
+++ analysis-pattern/src/test/java/accountability/PartyTest.java
@@ -2,21 +2,24 @@ package accountability;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class PartyTest {
     @Test
     public void simple_address_book_model() {
-        Person marlon = new Person("marlon",
+        Party marlon = new Person("marlon",
                 new TelephoneNumber("13511080333"),
                 new Address("大屯路2号院"),
                 new Email("yaolei@meituan.com")
         );
+        assertThat(marlon.getName()).isEqualTo("marlon");
         System.out.println("marlon = " + marlon);
 
-        Organization meituan = new Organization("meituan",
+        Party meituan = new Organization("meituan",
                 new TelephoneNumber("021-3333333"),
                 new Address("望京北路"),
                 new Email("aaaa@sankuai.com"));
-
+        assertThat(meituan.getName()).isEqualTo("meituan");
         System.out.println("meituan = " + meituan);
     }
 }
